56a5f87eafa35ee88efd88f37526a8c8cbd77a84
Epifanes <131815652+Epifanes@users.noreply.github.com>
2023-12-06 10:18:30 +0000

    Nonsense

--- Hello.py
+++ Hello.py
@@ -18,7 +18,7 @@ def main():
         'Fourier analysis overall': 'Images/Freq_spectrum.png',
         'Fourier analysis z-acceleration': 'Images/Fourier_z.png',
         'Fourier analysis y-acceleration': 'Images/Fourier_y.png',
-        'Fourier analysis x-acceleration': 'Images/Fourier_x.png',
+        'Fourier analysis x-acceleration': 'Images/Fourierx.png',
         'Spectogram z-acceleration': 'Images/Spectogram_z.png',
         'Spectogram y-acceleration': 'Images/Spectogram_y.png',
         'Spectogram x-acceleration': 'Images/Spectogram_x.png'
